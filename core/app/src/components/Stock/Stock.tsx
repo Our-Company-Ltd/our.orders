40a385a09e3a095df14cd71b33cdad468a742896
DiogoVaz <diogo@ourcompany.ch>
2020-10-12 11:33:51 +0100

    fix problem with stocks

--- core/app/src/components/Stock/Stock.tsx
+++ core/app/src/components/Stock/Stock.tsx
@@ -19,6 +19,7 @@ import { IsAdminOrInRole } from 'src/_helpers/roles';
 import { InjectedAuthProps } from 'src/_context';
 import { Button } from '@material-ui/core';
 import { Delete } from '@material-ui/icons';
+import { debounce } from 'throttle-debounce';
 
 export type StockProps = InjectedSettingsProps & InjectedWarehouseProps & InjectedAuthProps & {
 
@@ -26,18 +27,18 @@ export type StockProps = InjectedSettingsProps & InjectedWarehouseProps & Inject
 };
 type State = {
     loading: boolean;
-    units: { [sku: string]: StockUnit | undefined }[];
+    units: StockUnit[];
     page: number;
 };
 
 export class Stock extends React.Component<StockProps, State> {
     constructor(props: StockProps) {
         super(props);
-        this._onChange = this._onChange.bind(this);
+        this._onChange = debounce(300, this._onChange);
         this._onDelete = this._onDelete.bind(this);
         this._handleChangePage = this._handleChangePage.bind(this);
-        const units: { [sku: string]: StockUnit | undefined }[] = [];
-        props.skus.forEach(sku => units.push({ [sku.sku]: undefined }));
+        const units: StockUnit[] = [];
+        props.skus.forEach(sku => units.push({ Units: {}, SKU: sku.sku } as StockUnit));
         this.state = {
             loading: false,
             units: units,
@@ -63,12 +64,7 @@ export class Stock extends React.Component<StockProps, State> {
                     <TableHead>
                         <TableRow>
 
-                            <TableCell
-
-                                numeric={false}
-                            >
-                                Product name
-                            </TableCell>
+                            <TableCell numeric={false}>Product name</TableCell>
                             {warehouseCtx.Warehouses.map((s, i) => {
                                 return (
                                     <TableCell
@@ -79,62 +75,55 @@ export class Stock extends React.Component<StockProps, State> {
                                     </TableCell>
                                 );
                             })}
-                            <TableCell>
-                                delete
-                            </TableCell>
+                            <TableCell>delete</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {skus
-                            .filter((sku, i) => i >= page * rowsPerPage && i < (page + 1) * rowsPerPage)
-                            .map(sku => {
-                                const title = sku.legend || sku.sku;
-                                return units.map(u => {
-                                    const unit = u[sku.sku] ||
-                                        { Id: '', Name: '', SKU: sku.sku, Units: {}, Detail: '' } as StockUnit;
-                                    const { SKU } = unit;
+                        {units
+                            .filter((unit, i) => i >= page * rowsPerPage && i < (page + 1) * rowsPerPage)
+                            .map(unit => {
+                                const l = skus.find(s => s.sku === unit.SKU);
+                                const title = l && l.legend || l && l.sku;
+                                const { SKU } = unit;
 
-                                    return (<TableRow
-                                        hover={true}
-                                        role="checkbox"
-                                        tabIndex={-1}
-                                        key={SKU}
-                                    >
-                                        <TableCell component="th" scope="row" padding="none">
-                                            {title}
-                                        </TableCell>
-                                        {warehouseCtx.Warehouses.map((s) => {
-                                            var c = unit.Units && unit.Units[s.Id];
-                                            return (
-                                                <TableCell
-                                                    key={s.Id}
-                                                    numeric={true}
-                                                >
-                                                    <TextField
-                                                        value={c === null ? '' : c}
-                                                        type="number"
-                                                        fullWidth={true}
-                                                        disabled={!hasRights}
-                                                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                                                            var val = e.target.value;
-                                                            var stock = val === '' ? null : parseInt(val, 10);
-                                                            this._onChange(SKU, s.Id, stock);
-                                                        }}
-                                                    />
-                                                </TableCell>
-                                            );
+                                return (<TableRow
+                                    hover={true}
+                                    role="checkbox"
+                                    tabIndex={-1}
+                                    key={SKU}
+                                >
+                                    <TableCell component="th" scope="row" padding="none">
+                                        {title}
+                                    </TableCell>
+                                    {warehouseCtx.Warehouses.map((s) => {
+                                        var c = unit.Units && unit.Units[s.Id];
+                                        return (
+                                            <TableCell
+                                                key={s.Id}
+                                                numeric={true}
+                                            >
+                                                <TextField
+                                                    value={c === null ? '' : c}
+                                                    type="number"
+                                                    fullWidth={true}
+                                                    disabled={!hasRights}
+                                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                                        var val = e.target.value;
+                                                        var stock = val === '' ? null : parseInt(val, 10);
+                                                        this._onChange(unit, s.Id, stock);
+                                                    }}
+                                                />
+                                            </TableCell>
+                                        );
 
-                                        })}
-                                        <TableCell>
-                                            <Button onClick={() => this._onDelete(unit)}>
-                                                <Delete />
-                                            </Button>
-                                        </TableCell>
-                                    </TableRow>);
-                                });
-                            }
-                            )}
-
+                                    })}
+                                    <TableCell>
+                                        <Button onClick={() => this._onDelete(unit)}>
+                                            <Delete />
+                                        </Button>
+                                    </TableCell>
+                                </TableRow>);
+                            })}
                     </TableBody>
                 </Table>
                 {rowCount > rowsPerPage && <TablePagination
@@ -166,28 +155,49 @@ export class Stock extends React.Component<StockProps, State> {
         this.setState(() => ({ loading: true }), () => {
             StockUnits.Find(def, 0, 999999)
                 .then(result => {
-                    const newUnits = result.Values.map(v => ({ [v.SKU]: v }));
-                    if (newUnits.length > 0) {
-                        this.setState(() => ({ units: newUnits, loading: false }));
-                    }
+                    this.setState((prev) => {
+                        const newUnits = [];
+                        for (let i = 0; i < result.Values.length; i++) {
+                            const unit = result.Values[i];
+                            const similar = prev.units.findIndex(u => u.SKU === unit.SKU);
+                            if (similar >= 0) {
+                                prev.units.splice(similar, 1);
+                            }
+                            newUnits.push(unit);
+                        }
+
+                        return { loading: false, units: [...prev.units, ...newUnits] };
+                    });
                 });
         });
 
     }
-    private _onChange(sku: string, shopId: string, value: number | null) {
+    private _onChange(unit: StockUnit, shopId: string, value: number | null) {
         const { units } = this.state;
 
-        const old = units[sku] || { SKU: sku, Units: {} } as StockUnit;
+        const old = units.find(u => u === unit);
+        if (!old) {
+            // tslint:disable-next-line:no-console
+            console.error('problem finding unit');
+            return;
+        }
+
         old.Units[shopId] = value || 0;
 
         this.setState(() => ({ loading: true }), () => {
             if (old.Id) {
                 StockUnits.Patch(old.Id, old).then((newUnit) => {
-                    this.setState((prev) => ({ loading: false, units: { ...prev.units, [sku]: newUnit } }));
+                    this.setState((prev) => {
+                        prev.units.splice(prev.units.indexOf(old), 1, newUnit);
+                        return { loading: false, units: [...prev.units] };
+                    });
                 });
             } else {
                 StockUnits.Create(old).then((newUnit) => {
-                    this.setState((prev) => ({ loading: false, units: { ...prev.units, [sku]: newUnit } }));
+                    this.setState((prev) => {
+                        prev.units.splice(prev.units.indexOf(old), 1, newUnit);
+                        return { loading: false, units: [...prev.units] };
+                    });
                 });
             }
         });
@@ -199,14 +209,18 @@ export class Stock extends React.Component<StockProps, State> {
         var confirm = window.confirm('Do you want to continue ?');
 
         this.setState(() => ({ loading: true }), () => {
-
-            if (confirm) {
+            if (confirm && unit.Id) {
                 StockUnits.Delete(unit.Id).then(() => {
-                    const updatedUnits = units.filter(u => u[unit.SKU] !== unit);
-                    this.setState((prev) => ({ loading: false, units: updatedUnits }));
+                    const updatedUnits = units.filter(u => u !== unit);
+                    this.setState(() => ({ loading: false, units: updatedUnits }));
                 });
             }
 
+            // to be explicit
+            if (confirm && !unit.Id) {
+                const updatedUnits = units.filter(u => u !== unit);
+                this.setState(() => ({ loading: false, units: updatedUnits }));
+            }
         });
 
     }
